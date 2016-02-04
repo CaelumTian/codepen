72c50fe48e48197e881ae35238efb9c1643f91b9
T-phantom <758962035@qq.com>
2016-02-04 22:28:01 +0800

    reflux

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@
  */
 import "../styles/reset.less";
 import React from 'react';
+import Reflux from 'reflux';
 
 const Todo = React.createClass({
     getInitialState() {
@@ -42,4 +43,55 @@ const Todo = React.createClass({
         )
     }
 });
-React.render(<Todo />, document.body);
+
+var TodoActions = Reflux.createActions([
+    'addItem', 'getAll'
+]);
+var TodoStore = Reflux.createStore({
+    items : ['第1条数据', '第2条数据', '第3条数据'],
+    listenables : [TodoAction],
+    onAddItem() {
+        console.log("请求服务器更新数据");
+        var newItem = '第' + (this.items.length + 1) + '条数据';
+        this.items.push(newItem);
+        this.trigger(this.items);
+    },
+    onGetAll() {
+        console.log("获取数据");
+        this.trigger(this.items);
+    }
+});
+var Todolist = React.createClass({
+    getInitialState() {
+        return {
+            list : []
+        }
+    },
+    onStatusChange(list) {
+        this.setState({
+            list : list
+        })
+    },
+    handleClick() {
+        TodoAction.addItem();
+    },
+    componentDidMount() {
+        this.unsubscribe1 = TodoStore.listen(this.onStatusChange);
+        TodoAction.getAll();
+    },
+    componentWillUnmount() {
+        this.unsubscribe1();
+    },
+    render() {
+        return (
+            <div>
+                <h1>数据在这里</h1>
+                {this.state.list.map(item => {
+                    return <p>{item}</p>
+                })}
+                <button onClick={this.handleClick}>添加项目</button>
+            </div>
+        )
+    },
+});
+React.render(<Todolist />, document.body);
